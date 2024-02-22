390d69e204f02c67b7a3bdd857f7782eb80b18c7
sba23150 <sba23150@student.cct.ie>
2024-02-22 20:25:22 +0000

    Initialized 2 constructor part A of the CA2

--- CA2/src/ca2/CA2.java
+++ CA2/src/ca2/CA2.java
@@ -7,13 +7,36 @@ package ca2;
 /**
  *
  * @author Tatiana Mota - sba23150
- * 
+ * gitHub: https://github.com/sba23150/CA2.git
  */
-public class CA2 {
 
-    /**
-     * @param args the command line arguments
-     */
+/**(A)	Write the complete class, Employee, which has the following fields and methods:
+Instance fields:
+name, of type String
+email, of type String
+empNum, of type int
+*/
+
+public class CA2 {
+    //initialing the instance fields
+    private String name;
+    private String email;
+    private empNum int;
+    
+    //initializing constructor 1 for default values:
+    public CA2(){
+        this.name = "Default";
+        this.email = "Default";
+        this.empNum = 0;
+    }
+    //constructor with values
+    public CA2 (String name, String email, int empNum){
+        this.name = name;
+        this.email = email;
+        this.empNum = empNum;
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
