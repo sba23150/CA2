c232a59e7b54ce2c0041b9b91e15adf20c466e48
sba23150 <sba23150@student.cct.ie>
2024-02-27 20:37:33 +0000

    Complete Part 2 - (a)

--- CA2/src/ca2/Company.java
+++ CA2/src/ca2/Company.java
@@ -4,6 +4,9 @@
  */
 package ca2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Tatiana Mota - sba23150
@@ -16,20 +19,39 @@ Company class stores information about employees.
 
 public class Company {
     //initializing the field companyName for the name of the company
-    private String companyName = "Business Gnómes Ltd";
+    private String companyName;
     //initializing an ArrayList, called staff, to hold a list of employee objects, whose class is described in Question 1 above
-    String [] staff;
+    private ArrayList<Employee> staff;
     
     //Write the default constructor for this class which initialises all fields.
     public Company(){
-        
+        companyName = "Business Gnómes Ltd";
+        staff = new ArrayList<>();
     }
     
     //Write an overloaded constructor which accepts a name value as a parameter
-    public Company (String name, String email){
-        
-        
+    public Company (String companyName){
+        this.companyName = companyName;
+        staff = new ArrayList<>();        
+    }    
+    // Method addNewStaff() that adds a new employee to the staff arrayList (the employee is a parameter). 
+    public void addNewStaff(Employee employee) {
+        staff.add(employee);
     }
-    
-            
+
+    // Method getStaffNumber() that returns the number of employees currently in the arrayList.
+    public int getStaffNumber() {
+        return staff.size();
+    }
+
+    // Method listEmployees() that uses an iterator object to find in the arrayList all employees above a given employee number value  (this value is a parameter for this method) and prints the names of these employees.
+    public void listEmployees(int employeeNumber) {
+        Iterator<Employee> iterator = staff.iterator();
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+            if (employee.getEmpNum() > employeeNumber) {
+                System.out.println(employee.getName());
+            }
+        }
+    }     
 }
