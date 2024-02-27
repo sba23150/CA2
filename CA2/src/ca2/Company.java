d1005d1ffa9fc5fcda1ac6d3a40474e9cd2b92b5
sba23150 <sba23150@student.cct.ie>
2024-02-27 20:12:17 +0000

    Created Company class to store information about employees

---
+++ CA2/src/ca2/Company.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca2;
+
+/**
+ *
+ * @author Tatiana Mota - sba23150
+ * gitHub: https://github.com/sba23150/CA2.git
+ */
+
+/**Part 2 
+Company class stores information about employees.
+*/
+
+public class Company {
+    //initializing the field companyName for the name of the company
+    private String companyName = "Business Gnómes Ltd";
+    //initializing an ArrayList, called staff, to hold a list of employee objects, whose class is described in Question 1 above
+    String [] staff;
+    
+    //Write the default constructor for this class which initialises all fields.
+    public Company(){
+        
+    }
+    
+    //Write an overloaded constructor which accepts a name value as a parameter
+    public Company (String name, String email){
+        
+        
+    }
+    
+            
+}
